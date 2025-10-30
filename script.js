f41f54823dc7d7e9fe71ce7c5716e72549b90e04
Abderrazak <aamaichabderrazak2004@gmail.com>
2025-10-30 15:03:50 +0100

    finition de html & css

---
+++ script.js
@@ -0,0 +1,7 @@
+const question = [
+    ["Quelle est la capitale du Maroc ?","Rabat","Algeria","Paris","Berlin","Rabat"],
+    ["Combien font 9² ?","11","18","81","64"],
+    ["Quelle est la langue officielle de Allemagne ?","Arabic","Français","Anglais","Allmand"],
+    ["Quel est locéan à lest de lAfrique ?","Océan Indien","Océan Atlantique","Océan Pacefique","Océan Arctique","Océan Indien"],
+    ["Combien de jours y a til au mois de février année non bissextile ?","30","29","28","31"]
+]   
